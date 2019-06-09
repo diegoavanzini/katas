6aad9ca0b5659992418931b353d816f67429eaca
diego <diego.avanzini@gmail.com>
2019-06-09 05:52:29 +0200

    green, empty maze test

--- tinymaze.go
+++ tinymaze.go
@@ -1,10 +1,14 @@
 package tinymaze
 
+import "errors"
 
 type TinyMazeSolver struct {
 
 }
 
 func (solver *TinyMazeSolver) Solve(tinyMaze [][]string) ([][]string, error) {
+	if len(tinyMaze) == 0 {
+		return [][]string{}, errors.New("empty maze")
+	}
 	return [][]string{}, nil
 }
