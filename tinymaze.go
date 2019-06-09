3b40409a85d4b40b244710ef624e1d3298c0c748
diego <diego.avanzini@gmail.com>
2019-06-09 06:00:59 +0200

    green - maze with one row and one columns should return error

--- tinymaze.go
+++ tinymaze.go
@@ -10,5 +10,8 @@ func (solver *TinyMazeSolver) Solve(tinyMaze [][]string) ([][]string, error) {
 	if len(tinyMaze) == 0 {
 		return [][]string{}, errors.New("empty maze")
 	}
+	if len(tinyMaze) == 1 && len(tinyMaze[0]) == 1 {
+		return [][]string{}, errors.New("maze should have Start and End")
+	}
 	return [][]string{}, nil
 }
